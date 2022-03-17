437a79b9b567ede21c1956e4510a4451c5d89c0f
takumi <t.takata121@outlook.jp>
2022-03-17 22:08:57 +0900

    #28

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<title><?php the_title(); ?></title>
 
 <!-- Bootstrap core CSS -->
 <link href="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
