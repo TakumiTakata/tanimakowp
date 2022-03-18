f7edeb19fb3ca4d100ebeebab546548a61234ad4
takumi <t.takata121@outlook.jp>
2022-03-19 07:08:17 +0900

    #30

--- page.php
+++ page.php
@@ -11,43 +11,41 @@
 
   <?php get_template_part('includes/header'); ?>
 
+
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <!-- Page Header -->
       <?php
       $img = get_eyecatch_with_default();
       ?>
-      <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
+      <header class="masthead" style="background-image: url('img/about-bg.jpg')">
         <div class="overlay"></div>
         <div class="container">
           <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
-              <div class="post-heading">
+              <div class="page-heading">
                 <h1><?php the_title(); ?></h1>
-                <span class="meta">Posted by
-                  <?php the_author(); ?>
-                  on <?php the_date(); ?></span>
               </div>
             </div>
           </div>
         </div>
       </header>
 
-      <!-- Post Content -->
-      <article>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              <?php the_post_thumbnail(); ?>
-              <?php the_content(); ?>
-            </div>
+      <!-- Main Content -->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <?php the_content(); ?>
           </div>
         </div>
-      </article>
+      </div>
 
       <hr>
+
+
     <?php endwhile; ?>
   <?php endif; ?>
+
 
   <?php get_template_part('includes/footer'); ?>
 
